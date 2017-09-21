ff26e68ada90d8f38188d18103131a85a1c6f835
traning <suvratagg@gmail.com>
2017-09-21 18:45:23 +0530

    S&G applied to location change form and service

--- client/src/app/components/centralaccess/accesstype/locationchange/locationchange.component.ts
+++ client/src/app/components/centralaccess/accesstype/locationchange/locationchange.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationChangeService } from './location-change.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-locationchange',
