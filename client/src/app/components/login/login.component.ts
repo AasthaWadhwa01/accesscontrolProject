82ee27735da9c4ebfff9939eccc686e6092b8d15
shubhang.5.gupta <shubhang.5.gupta@niit-tech.com>
2017-10-09 15:25:13 +0530

    localstorage

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -32,12 +32,9 @@ errors:string;
           this.errors = error;
         })
 
-/*     this.login.setToken().subscribe(data=>{
-     this.data=data;
-     let use = JSON.parse(localStorage.getItem(data.role));
-    })*/
+     this.login.setValue(this.role)
  }
-
+ use = JSON.parse(localStorage.getItem('role'));
 redir(empid:any) {
 	
  this.router.navigate(['/accessforms', empid])
